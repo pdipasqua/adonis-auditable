d38d34c0ddeb301bc71fb3aeadd2c8844a6df81f
Paolo Di Pasqua <pdipasqua@users.noreply.github.com>
2019-02-19 22:44:04 +0100

    Update Auditable for Prixacard

--- src/Traits/Auditable.js
+++ src/Traits/Auditable.js
@@ -35,12 +35,12 @@ function createWithAudit ({request, auth}) {
     const model = await this.create(data)
     const newModel = (await this.find(model.primaryKeyValue))
     const auditable = newModel.constructor.name
-    const auditableId = newModel.id
+    const auditableUuid = newModel.uuid
     const newData = newModel.$attributes
     const event = Audit.events.CREATE
 
     // save audit
-    await createAudit(event, {request, auth}, auditable, auditableId, null, newData)
+    await createAudit(event, {request, auth}, auditable, auditableUuid, null, newData)
 
     return model
   }
@@ -56,7 +56,7 @@ function createWithAudit ({request, auth}) {
 function updateWithAudit ({request, auth}) {
   return async function (data, ignoreDiff = ['updated_at']) {
     const auditable = this.constructor.name
-    const auditableId = this.id
+    const auditableUuid = this.uuid
     const oldData = this.$originalAttributes
     this.merge(data)
     const result = await this.save()
@@ -76,7 +76,7 @@ function updateWithAudit ({request, auth}) {
     const event = Audit.events.UPDATE
 
     // save audit
-    await createAudit(event, {request, auth}, auditable, auditableId, oldData, newData)
+    await createAudit(event, {request, auth}, auditable, auditableUuid, oldData, newData)
 
     return result
   }
@@ -92,12 +92,12 @@ function updateWithAudit ({request, auth}) {
 function deleteWithAudit ({request, auth}) {
   return async function () {
     const auditable = this.constructor.name
-    const auditableId = this.id
+    const auditableUuid = this.uuid
     const oldData = this.$originalAttributes
     const result = await this.delete()
 
     // save audit
-    await createAudit(Audit.events.DELETE, {request, auth}, auditable, auditableId, oldData)
+    await createAudit(Audit.events.DELETE, {request, auth}, auditable, auditableUuid, oldData)
 
     return result
   }
@@ -109,27 +109,36 @@ function deleteWithAudit ({request, auth}) {
  * @param event
  * @param oldData
  * @param auditable
- * @param auditableId
+ * @param auditableUuid
  * @param newData
  * @param auth
  * @param request
  * @returns {Promise<void>}
  */
-async function createAudit (event, {request, auth}, auditable, auditableId, oldData, newData) {
+async function createAudit (event, {request, auth}, auditable, auditableUuid, oldData, newData) {
   // check request was passed
   if (!request) {
     throw new Error('Request param is empty')
   }
 
+  var entity = null
+  if(auth.authenticatorInstance._config.model == 'App/Models/User') {
+	  entity = 'user'
+  } else {
+	  entity = 'customer'
+  }
+  
   // get user data to store
-  const userId = _.get(auth, 'user.id', null)
+  const AuthUserUuid = _.get(auth, 'user.uuid' , null)
+
   const url = request.originalUrl()
   const ip = request.ip()
 
   // save audit
   await Audit.create({
-    user_uuid: userId,
-    auditable_uuid: auditableId,
+    user_uuid: entity =='user' ? AuthUserUuid : null,
+    customer_uuid: entity =='customer' ? AuthUserUuid : null,
+    auditable_uuid: auditableUuid,
     auditable,
     event,
     url,
